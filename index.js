debda4f6f86eeb4fd35dd4e2a9bf05fcd26cab97
seemonz <seemonz@gmail.com>
2016-01-17 20:34:58 -0800

    add sockets

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-var express = require('express');
+var express = require("express");
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+http.listen(3000, function(){
+  console.log('Example app listening on port 3000!');
+});
+app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.sendfile('public/index.html');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+io.on('connection', function(socket){
+  console.log('a user has connected');
 });
